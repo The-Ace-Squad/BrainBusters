6a5ee06325d6cf8b93a5112ee629ff5095cc1a29
romeoyutig <romeoyutig2@outlook.com>
2023-02-14 15:37:15 -0800

    removed console logs

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -9,11 +9,6 @@ const App = ({
   sign_in_route,
   sign_out_route
 }) => {
-  console.log("logged_in:", logged_in)
-  console.log("current_user:", current_user)
-  console.log("new_user_route:", new_user_route)
-  console.log("sign_in_route:", sign_in_route)
-  console.log("sign_out_route:", sign_out_route)
   return (
     <>
       <h1>BrainBusters App</h1>
