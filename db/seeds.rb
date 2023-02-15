d658b2ea4d489b7a51e2917f321dfc5873f25e9a
romeoyutig <romeoyutig2@outlook.com>
2023-02-15 12:23:57 -0800

    removed user_id from post seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,14 +11,12 @@ user2 = User.create(
 
 posts = [
   {
-    user_id: 1,
     post_title: "Week two assessment problem three code challenge",
     post_content: "Please share answer for problem three on week two assessment",
     category_tag: "assessments",
     create_date: "14-02-2023"
   },
   {
-    user_id: 2,
     post_title: "Bo's Peanutbutter Noodles",
     post_content: "Hello Hotel Homies, I just realized I never sent over that recipe we spoke about the other day, so here it is!
     Bo's Spicy Peanut Butter Noodles:
