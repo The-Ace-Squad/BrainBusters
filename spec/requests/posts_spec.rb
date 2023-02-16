c6e5d98659cc1fab8a39104aaf3ef496972be0c9
romeoyutig <romeoyutig2@outlook.com>
2023-02-16 14:00:00 -0800

    completed create method and rspec tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -18,6 +18,23 @@ RSpec.describe "Posts", type: :request do
       expect(post.length).to eq 1
     end     
   end
-
-
+  describe "POST /create" do
+    it "creates a post" do
+      user = User.create(email:"test1@example.com", password:"123456", username:"exampletester")
+      post_params= {
+        post: {
+          user_id: user.id,
+          post_title: "Notes on Conditionals",
+          post_content: "Please share some notes on conditionals",
+          category_tag: "Notes",
+          create_date: "16-02-2023",
+        }
+      }
+      post "/posts", params: post_params
+      expect(response).to have_http_status(200)
+      
+      post2=Post.first
+      expect(post2.post_title).to eq("Notes on Conditionals")
+    end   
+  end  
 end
