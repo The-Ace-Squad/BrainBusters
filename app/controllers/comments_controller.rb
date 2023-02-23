db2171a84ec2ec18b520bb7da3e2dfe022138c16
romeoyutig <romeoyutig2@outlook.com>
2023-02-23 13:33:03 -0800

    completed testing and controller methods for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,7 +13,22 @@ class CommentsController < ApplicationController
     end
   end
 
+  def update
+    comment = Comment.find(params[:id])
+    comment.update(comment_params)
+    if comment.valid?
+      render json: comment
+    else
+      render json: comment.errors, status: 422
+    end
+  end
 
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    render json: Comment.all
+
+  end
   private
   def comment_params
       params.require(:comment).permit(:content, :post_id, :user_id, :vote_count)
--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,7 +1,93 @@
 require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
+  let(:user) { User.create email:"test1@example.com", password:"123456", username:"exampletester" }
+  let(:faux_post) {user.posts.create(
+    post_title: "Notes on Conditionals",
+    post_content: "Please share some notes on conditionals",
+    category_tag: "Notes",
+    create_date: "16-02-2023"
+  )}
+
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of comments" do
+      comment = Comment.create(
+        content: "Refer to the syllabus",
+        post_id: faux_post.id,
+        user_id: user.id,
+        vote_count:0
+      )
+
+      get '/comments'
+
+      comment = JSON.parse( response.body)
+      expect(response).to have_http_status(200)
+      expect(comment.length).to eq 1
+      
+    end
+  end
+  describe "POST /create" do
+    it "creates a comment" do
+      comment_params= {
+        comment: {
+          content: "Refer to the syllabus",
+          post_id: faux_post.id,
+          user_id: user.id,
+          vote_count:0
+        }
+      }
+      post "/comments", params: comment_params
+      expect(response).to have_http_status(200)
+      
+      comment2 = Comment.first
+      expect(comment2.content).to eq("Refer to the syllabus")
+    end
+  end
+  describe "PATCH /update" do
+    it "updates a comment" do
+      comment_params= {
+        comment: {
+          content: "Refer to the syllabus",
+          post_id: faux_post.id,
+          user_id: user.id,
+          vote_count:0
+        }
+      }
+      post "/comments", params: comment_params
+      comment = Comment.first
+
+      updated_comment_params= {
+        comment: {
+          content: "Refer to the notes",
+          post_id: faux_post.id,
+          user_id: user.id,
+          vote_count:0
+        }
+      }
+      patch "/comments/#{comment.id}", params: updated_comment_params
+      comment3 = Comment.last
+      expect(response).to have_http_status(200)
+      expect(comment3.content).to eq("Refer to the notes")
+    end
+  end
+  describe "DELETE /destroy" do
+    it "deletes a comment" do
+      comment_params= {
+        comment: {
+          content: "Refer to the syllabus",
+          post_id: faux_post.id,
+          user_id: user.id,
+          vote_count:0
+        }
+      }
+      post '/comments', params: comment_params
+      comment = Comment.first
+
+      delete "/comments/#{comment.id}"
+
+      comments = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(comments.length).to eq 0
+    end
   end
 end
