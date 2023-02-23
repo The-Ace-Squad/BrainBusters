ed7f88263bed381d57a6591f63f90c2fa2755df4
romeoyutig <romeoyutig2@outlook.com>
2023-02-23 14:14:30 -0800

    create comments component in development

---
+++ app/javascript/components/components/Comments.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from "react";
+import { useParams } from "react-router-dom";
+
+const Comments = () => {
+  // const { id } = useParams()
+  // const currentComment = comments?.filter((comment) => comment.id === +id)
+  // const [comments, setComments]= useState([])
+
+  // useEffect(() => {
+  //   readComments()
+  // }, [])
+  
+  // const readComments = () => {
+  //   fetch("/comments")
+  //     .then((response) => response.json())
+  //     .then((payload) => {
+  //       setComments(payload)
+  //     })
+  //     .catch((error) => console.log(error))
+  // }
+  return (
+    <>
+    <h1>Hello</h1>
+    </>
+  )
+}
+export default Comments
