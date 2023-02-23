f17bbd1feaebd32f9a8e3c1218dbb18265aa9d78
msawhitney <msawhit3@gmail.com>
2023-02-23 11:22:36 -0800

    about-us styling and testing

---
+++ app/javascript/components/pages/AboutUs.js
@@ -0,0 +1,31 @@
+import React from "react"
+
+const AboutUs = () => {
+  return (
+    <>
+        <div className="about-heading">
+            <h2>About Us</h2>
+        </div> 
+        <div className="about-matt">
+            <h3>Product Manager: Matthew Munoz</h3>
+               
+            <p>Hey, I'm Matthew Munoz from San Diego, California. I'm a former high school teacher turned full-stack developer. When I'm not coding I enjoy playing video games, finding great restaurants, boxing, and spending time with my friends and family.</p>
+        </div> 
+        <div className="about-natally">    
+            <h3>Project Manager: Natally Chaves Quintero</h3>
+            <p>Hey, I'm Natally Chaves Quintero from Orlando, Florida. I'm passionate about coding and love turning complex problems into elegant solutions. When I'm not coding, I enjoy exploring local spots and spending time with family and friends. My goal is to use my skills to make a positive impact on the world.</p>
+        </div>
+        <div className="about-romeo">
+          <h3>Tech Lead: Romeo Yutig</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere ut velit vitae feugiat. Aliquam ut interdum enim. Cras faucibus est non interdum commodo. Cras sed massa ultrices, mattis nibh lacinia, vehicula augue. Sed vitae tellus non massa blandit pellentesque fermentum a orci. Nunc id metus quis lacus pharetra molestie. Proin in libero ac ante accumsan tincidunt non et enim. Ut suscipit aliquet enim. Curabitur vel consectetur nulla.</p>
+        </div>
+        <div className="about-awhit">  
+            <h3>Design Lead: A. Whitney</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere ut velit vitae feugiat. Aliquam ut interdum enim. Cras faucibus est non interdum commodo. Cras sed massa ultrices, mattis nibh lacinia, vehicula augue. Sed vitae tellus non massa blandit pellentesque fermentum a orci. Nunc id metus quis lacus pharetra molestie. Proin in libero ac ante accumsan tincidunt non et enim. Ut suscipit aliquet enim. Curabitur vel consectetur nulla.</p>
+        </div> 
+
+    </>
+  )
+}
+
+export default AboutUs
