e2f72ffa05da2bbe5e4588a78ff712c53b75e61c
Nata19754 <chavesnatally0419@gmail.com>
2023-02-27 10:22:52 -0800

    worked on styling

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -4,7 +4,9 @@ import logoimage from "../assets/logoimage.png"
 const Home = () => {
   return (
     <>
-
+      <div className="home-img">
+        <img src={logoimage} alt=" Logo Image" />
+      </div>
       <div className="home-para">
           <p> Brain Busters is the innovative solution for anyone looking to unleash their full potential and bring their ideas to life. With its easy-to-use interface, you can quickly and effortlessly store, share, and collaborate on all of your notes and thoughts. Whether you're a student, professional, or just someone with a lot of ideas, Brain Busters makes it simple to keep track of everything in one place. So why let your mind be the limit when you can bust it wide open with Brain Busters. SIGN UP TODAY!! </p>
       </div>
